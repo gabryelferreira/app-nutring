09cbfeb8fdaf6e120806656657d3c32243874a3d
gabryelferreira <gabryel.ferreira@hotmail.com>
2018-09-15 22:03:02 -0300

    Alterando home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -20,12 +20,32 @@ export class HomePage {
 
   selectedTheme: String = "";
   user: IUser = {
-    altura_m: "", cep: "", dt_nasc: "", email: "", foto: "", id_usuario: 0, nome: "", peso_kg: "", senha: "", sexo: "", sobrenome: "", telefone: "", usuario: ""
+    altura_m: "", 
+    cep: "", 
+    dt_nasc: "", 
+    email: "", 
+    foto: "", 
+    id_usuario: 0, 
+    nome: "", 
+    peso_kg: "", 
+    senha: "", 
+    sexo: "", 
+    sobrenome: "", 
+    telefone: "", 
+    usuario: ""
   };
   kcalMeta:number = 0;
   kcalConsumida:number;
   imgTeste = "http://www.leisureopportunities.com/images/247215_898310.jpg";
   sizeTeste = "small";
+  rate: number = 3.55245;
+  stars = [
+    {star: 'md-star-outline', value: 1},
+    {star: 'md-star-outline', value: 2},
+    {star: 'md-star-outline', value: 3},
+    {star: 'md-star-outline', value: 4},
+    {star: 'md-star-outline', value: 5}
+]
 
   constructor(private tabs:Tabs,public navCtrl: NavController, public navParams: NavParams, 
               private _settings: SettingsService, private post:service.HomePostService, private get:service.HomeGetService) {
@@ -34,6 +54,26 @@ export class HomePage {
     let nome = this.user.nome.split(' '); 
     this.user.nome =  nome[0] + " " +nome[1]
     this.post.getDailyKcal(this.user.id_usuario).then((kcalMeta)=> this.kcalMeta= kcalMeta.result.kcal_diaria)
+    this.validateRate();
+  }
+
+  validateRate(){
+    let full = 'md-star';
+    let half = 'md-star-half';
+    let none = 'md-star-outline';
+
+    let rate = this.rate;
+
+    this.stars.forEach(element => {
+      if (element.value <= rate + 0.25)
+        element.star = full;
+      else if (element.value <= rate + 0.75)
+        element.star = half;
+      else
+        element.star = none;
+    });
+
+
   }
 
   async ionViewWillEnter() {
