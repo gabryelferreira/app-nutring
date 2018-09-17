577ac20d4655fb9165aac9e85bcc6db09d97b3c9
gabryelferreira <gabryel.ferreira@hotmail.com>
2018-09-16 22:20:10 -0300

    Removendo pipe

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { NtNumberPipe } from './nt-number/nt-number';
 import { NtMonthPipe } from './nt-month/nt-month';
 import { NtNomePratoPipe } from './nt-nome-prato/nt-nome-prato';
-import { NtPorcaoPipe } from './nt-porcao/nt-porcao';
 @NgModule({
 	declarations: [NtNumberPipe,
     NtMonthPipe,
-    NtNomePratoPipe,
-    NtPorcaoPipe],
+    NtNomePratoPipe],
 	imports: [],
 	exports: [NtNumberPipe,
     NtMonthPipe,
-    NtNomePratoPipe,
-    NtPorcaoPipe]
+    NtNomePratoPipe]
 })
 export class PipesModule {}
