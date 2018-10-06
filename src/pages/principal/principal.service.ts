c146430dfcdfb271898353ffcb4cb7b21f7e3ea1
gabryelferreira <gabryel.ferreira@hotmail.com>
2018-10-06 16:17:57 -0300

    Adicionando tela de refeicoes

---
+++ src/pages/principal/principal.service.ts
@@ -0,0 +1,38 @@
+import { Connect } from "../../app/connect";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable()
+export class PrincipalGetService extends Connect {
+    constructor(private _http: HttpClient) {
+        super(_http, "get");
+    }
+}
+
+@Injectable()
+export class PrincipalPostService extends Connect {
+    constructor(private _http: HttpClient) {
+        super(_http, "post");
+    }
+
+    postPicture(id, img, descricao) {
+        return this.callMethod("postPicture", { id, img, descricao });
+    }
+
+    getRefeicoesPadroes(){
+        return this.callMethod("getRefeicoesPadroes");
+    }
+
+    getRefeicoesCustom(id_usuario: number){
+        return this.callMethod("getRefeicoesCustom", { id_usuario });
+    }
+
+    createRefeicaoCustom(id_usuario: number, refeicao: string, foto?: any){
+        if (foto){
+            return this.callMethod("createRefeicaoCustom", { id_usuario, refeicao, foto });
+        } else {
+            return this.callMethod("createRefeicaoCustom", { id_usuario, refeicao });
+        }
+    }
+
+}
--- src/pages/principal/principal.ts
+++ src/pages/principal/principal.ts
@@ -1,22 +1,58 @@
 import { SettingsService } from './../settings/settings.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as service from './principal.service';
+import * as types from '../../app/types';
 
 @IonicPage()
 @Component({
   selector: 'page-principal',
   templateUrl: 'principal.html',
+  providers: [
+    service.PrincipalPostService
+  ]
 })
 export class PrincipalPage {
 
   selectedTheme: String = "";
+  refeicoesPadroes = [];
+  refeicoesCustom = [];
+  loading: boolean = false;
+  user: types.IUser = {};
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              _settings: SettingsService) {
+              _settings: SettingsService, private post: service.PrincipalPostService) {
     _settings.getActiveTheme().subscribe(val => (this.selectedTheme = val));
+    this.user = JSON.parse(localStorage.getItem("userData"));
+    this.getRefeicoes();
   }
 
   ionViewDidLoad() {
+  }
+
+  async getRefeicoes(){
+    this.loading = true;
+    let result = await this.getRefeicoesPadroes();
+    let result2 = await this.getRefeicoesCustom(this.user.id_usuario);
+    this.loading = false;
+  }
+
+  async getRefeicoesCustom(id_usuario: number){
+    let result = await this.post.getRefeicoesCustom(id_usuario);
+    if (result.success){
+      this.refeicoesCustom = result.result;
+    }
+  }
+
+  async getRefeicoesPadroes(){
+    let result = await this.post.getRefeicoesPadroes();
+    if (result.success){
+      this.refeicoesPadroes = result.result;
+    }
+  }
+
+  openCriarRefeicao(){
+
   }
 
 }
