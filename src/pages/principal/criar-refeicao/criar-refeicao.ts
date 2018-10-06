9effb6f709e86250019a476de5160f30c96519ff
gabryelferreira <gabryel.ferreira@hotmail.com>
2018-10-06 17:10:38 -0300

    Adicionando tela para criar refeição

---
+++ src/pages/principal/criar-refeicao/criar-refeicao.ts
@@ -0,0 +1,29 @@
+import { SettingsService } from './../../settings/settings.service';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the CriarRefeicaoPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-criar-refeicao',
+  templateUrl: 'criar-refeicao.html',
+})
+export class CriarRefeicaoPage {
+
+  selectedTheme: String = "";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              _settings: SettingsService) {
+    _settings.getActiveTheme().subscribe(val => (this.selectedTheme = val));
+  }
+
+  ionViewDidLoad() {
+  }
+
+}
